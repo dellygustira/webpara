9b4d4c4587fe9bc8a35228565f0850a12069e116
DELLY ASEP GUSTIRA <92769877+dellygustira@users.noreply.github.com>
2025-11-17 12:48:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Navbar scroll effect
+window.addEventListener('scroll',function () {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+    
+});
+
+//Active Link based on scroll position
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= (sectionTop - 100)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
